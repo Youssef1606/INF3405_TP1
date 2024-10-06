43a632087ba2ae3f4a55ee83dadf9b9a8bb5fc98
IlyasBensaha <ilyas.bensaha@polymtl.ca>
2024-10-06 00:46:16 -0400

    CLICommandDelete marche correctement avec les dossiers non-vides

--- Serveur/src/CLICommandDelete.java
+++ Serveur/src/CLICommandDelete.java
@@ -1,4 +1,10 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class CLICommandDelete extends CLICommand {
 	
@@ -7,20 +13,36 @@ public class CLICommandDelete extends CLICommand {
 	@Override public String execute(CLIApp app) {
 		String fileName = argGet(0);
 		File fileToDelete = new File(app.cdGet() + directorySeparator + fileName);
+		boolean isFile = fileToDelete.isFile();
 		
-		// TODO: delete folders with files inside
+		Path pathToDelete = fileToDelete.toPath();
 		
-		// if isFile() ... fichier ... supprimé!
-		
-		// if isDirectory() ... dossier ... supprimé!
-		
-		
-		if (!(fileToDelete.isFile()) && !(fileToDelete.isDirectory())) {
+		try {
+			// source du code [1] : https://stackoverflow.com/questions/779519/delete-directories-recursively-in-java
+			Files.walkFileTree(pathToDelete, new SimpleFileVisitor<Path>() {
+				   @Override
+				   public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				       Files.delete(file);
+				       return FileVisitResult.CONTINUE;
+				   }
+
+				   @Override
+				   public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				       Files.delete(dir);
+				       return FileVisitResult.CONTINUE;
+				   }
+			});
+			// fin du code de [1]
+		} catch (IOException e) {
+			e.printStackTrace();
 			return fileName + " is not a directory.";
 		}
 		
-		fileToDelete.delete();
-		
-		return "Successfully deleted " + fileName + "!";
+		if (isFile) {
+			return "Fichier " + fileName + " supprimé!";
+		}
+		else {
+			return "Dossier " + fileName + " supprimé!";
+		}
 	}
 }
